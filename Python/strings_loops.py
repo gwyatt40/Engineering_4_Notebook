791d2073b6174e7ab68f3a8c805f65c99e96accf
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2021-09-30 09:16:38 -0400

    Create strings_loops.py

---
+++ Python/strings_loops.py
@@ -0,0 +1,5 @@
+print ("Enter phrase") # initial instructions
+
+for x in input(): # takes input, input is string, so x is character in string
+    if x == ' ': print('-') # replaces spaces with - signs
+    else: print (x) # prints non space characters on new lines
