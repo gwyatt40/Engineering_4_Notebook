806c6e24ed850850f5c212289f753ae4d8a4633a
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2021-12-06 10:39:02 -0500

    commented shutdown code

--- Python/shutdown.py
+++ Python/shutdown.py
@@ -1,26 +1,15 @@
-# safe_restart_shutdown_interrupt_Pi.py
-#
-# Raspberry Pi Safe Restart and Shutdown Python Script
-# WRITTEN BY: Matthew Miller @ CHS
-# MODIFIED: 11/30/2021
-# DESCRIPTION: This python script uses a button to safely
-# reboot/shutdown your RPi. A momentary press reboots the pi,
-# holding the button shuts the RPi down.
-#
-# Based on code from the following tutorial:
-
-#-------------------------------------------------
+# import time and GPIO 
 import time
-import RPi.GPIO as GPIO
-# Pin definition
-reset_shutdown_pin = 18
-# Suppress warnings
+import RPi.GPIO as GPIO 
+# set pin 
+reset_shutdown_pin = 18 # changed from original pin
+# suppress warnings
 GPIO.setwarnings(False)
-# Use "GPIO" pin numbering
+# GPIO Mode for pin numbers 
 GPIO.setmode(GPIO.BCM)
-# Use built-in internal pullup resistor so the pin is not floating
+# use internal pull up resistor 
 GPIO.setup(reset_shutdown_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# modular function to restart Pi
+# define restart function 
 def restart():
     print("restarting Pi")
     command = "/usr/bin/sudo /sbin/shutdown -r now"
@@ -28,29 +17,36 @@ def restart():
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
     print(output)
-# modular function to shutdown Pi
+# define shutdown function 
 def shut_down():
     print("shutting down")
-    command = "/usr/bin/sudo /sbin/shutdown -h now"
+    command = "/usr/bin/sudo /sbin/shutdown -h now" #shutdown command 
     import subprocess
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
     print(output)
 while True:
+    # delay so doesn't use too much processing power 
     time.sleep(0.5)
+    # waits for button press w/ debounce 
     channel = GPIO.wait_for_edge(reset_shutdown_pin, GPIO.FALLING, bouncetime=200)
     if channel is None:
         print('Timeout occurred')
     else:
         print('Edge detected on channel', channel)
+        # For troubleshooting, uncomment this line to output button status on 
+command line
         #print('GPIO state is = ', GPIO.input(reset_shutdown_pin))
         counter = 0
         while GPIO.input(reset_shutdown_pin) == False:
+            # For troubleshooting, uncomment this line to view the counter. If it 
+reaches a value above 4, we will restart.
             #print(counter)
             counter += 1
             time.sleep(0.5)
-            # long button press
+            # long button press = shutdown 
             if counter > 4:
-                shut_down()
-        #if short button press, restart!
-        restart()
+                shut_down() # run shutdown function 
+        # short button press = restart
+        restart() # run restart function 
+	
