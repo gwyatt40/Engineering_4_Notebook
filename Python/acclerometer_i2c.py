e8c97afdac3e3dc51753fab4195217501a2dc916
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2022-01-30 18:55:10 -0500

    commented code

--- Python/acclerometer_i2c.py
+++ Python/acclerometer_i2c.py
@@ -1,36 +1,35 @@
 import time
+# import time libraries 
+import Adafruit_GPIO.SPI as SPI #import GPIO 
+import Adafruit_SSD1306 #import LCD library 
 
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
-
-import Adafruit_LSM303
-
+import Adafruit_LSM303 #import accelerometer library
+# image libraries for LCD: 
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
 font = ImageFont.load_default()
-
-# Raspberry Pi pin configuration:
+# reset pin: 
 RST = 24
-# Note the following are only used with SPI:
+
 DC = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
 
-
-# 128x32 display with hardware I2C:
+# display address set up and start: 
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST,i2c_address=0x3d)
 disp.begin()
 
 while True:
-    # Read the X, Y, Z axis acceleration values and print them.
+    # reads acceleration 
     accel, mag = lsm303.read()
-    # Grab the X, Y, Z components from the reading and print them out.
+    # sets acceleration values as variables
     accel_x, accel_y, accel_z = accel
     mag_x, mag_y, mag_z = mag
+    # writes acceleration values to LCD screen
     draw.text('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={$
           accel_x, accel_y, accel_z, mag_x, mag_y, mag_z), font=font, fill= 255)
-    
+    # pause before reading accelerometer values again 
     time.sleep(0.5) 
 
