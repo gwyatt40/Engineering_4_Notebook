205d6f13e2cf1cf8463c9337936eb76702d8b856
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2022-01-31 02:12:13 -0500

    commented stop motion code

--- Python/stopmotion/stopmotionvid.py
+++ Python/stopmotion/stopmotionvid.py
@@ -1,17 +1,17 @@
-from picamera import PiCamera
-from time import sleep
-from gpiozero import Button
+from picamera import PiCamera # imports camera libraries
+from time import sleep # imports time libaries 
+from gpiozero import Button # imports button libraries
 
-button = Button(17)
-camera = PiCamera()
+button = Button(17) # sets button pin 17
+camera = PiCamera() # sets camera 
 
-camera.start_preview()
-frame = 1
+camera.start_preview() # starts preview display 
+frame = 1 # sets intial frame #
 while True:
     try:
         button.wait_for_press()
-        camera.capture('frame%03d.jpg' % frame)
-        frame += 1
-    except KeyboardInterrupt:
-        camera.stop_preview()
+        camera.capture('frame%03d.jpg' % frame) # takes photo and names image with frame number
+        frame += 1 # increases frame number by one 
+    except KeyboardInterrupt: # KeyboardInterrupt stops camera
+        camera.stop_preview() # stops preview display 
         break
