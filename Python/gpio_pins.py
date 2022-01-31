925d861a151c9daeda5ad39854d11df9395b3139
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2022-01-31 02:34:13 -0500

    added commented gpio code

--- Python/gpio_pins.py
+++ Python/gpio_pins.py
@@ -1,71 +1,34 @@
-# I2C Intro
-# By Rowan & Meg
 import time
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
-import Adafruit_LSM303
+# import time libraries 
+import Adafruit_GPIO.SPI as SPI #import GPIO 
+import Adafruit_SSD1306 #import LCD library 
 
+import Adafruit_LSM303 #import accelerometer library
+# image libraries for LCD: 
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-# creates a lsm303 instance
-lsm303 = Adafruit_LSM303.LSM303()
-
-# pi pin configuration
+font = ImageFont.load_default()
+# reset pin: 
 RST = 24
-# DC = 23
-# SPI_PORT = 0
-# SPI_DEVICE = 0
-# i2cdetect -y 1 to find adress
 
-# 128x32 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
 
-# initializes library
+# display address set up and start: 
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST,i2c_address=0x3d)
 disp.begin()
 
-# clears the display
-disp.clear()
-disp.display()
-
-# create a blank image to draw on
-# mode '1' for one bit colour
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
-
-# drawing object draws on image
-draw = ImageDraw.Draw(image)
-
-# draws a black block to clear the screen
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-#padding makes resizing easier
-padding = 2
-top = padding
-bottom = height-padding
-x = 1
-
-# loads the default font
-font = ImageFont.load_default()
-
 while True:
-    # read X, Y, Z values
+    # reads acceleration 
     accel, mag = lsm303.read()
-    accel_x, accel_y, accel_z = accel
+    # sets acceleration values as variables
+    accel_x, accel_y, accel_z = accel # divide accel values by 10 for m/s^2
     mag_x, mag_y, mag_z = mag
-    # type in the accel results
-    
-    # draw a black box to prevent overlap
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
-    
-    draw.text((x, top),'Accel X={0}, Y={1}, Z={2}'.format(accel_x, accel_y, accel_z), font=font, fill=255)
-    # two different lines to prevent overlap
-    draw.text((x, top+10),'Mag X={0}, Y={1}, Z={2}'.format(mag_x, mag_y, mag_z), font=font, fill=255)
-
-    disp.image(image)
-    # disp.display() is needeed here
-    disp.display()
-    time.sleep(0.5)
-    disp.clear()
+    # writes acceleration values to LCD screen
+    draw.text('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={$
+          accel_x, accel_y, accel_z, mag_x, mag_y, mag_z), font=font, fill= 255)
+    # pause before reading accelerometer values again 
+    time.sleep(0.5) 
