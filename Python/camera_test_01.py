400ac34fe1adf8a7c26cabb20140745e65aa4d36
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2022-01-31 00:19:45 -0500

    commented camera test 01

--- Python/camera_test_01.py
+++ Python/camera_test_01.py
@@ -1,12 +1,12 @@
-import time
+import time #imports time libraries necessary for pauses
 
-import picamera
+import picamera #imports camera libraries 
 
-print("Running")
+print("Running") #constantly displayed while camera running 
 
 with picamera.PiCamera() as camera:
-	camera.resolution = (1024, 768)
-	camera.start_preview()
-	time.sleep(2)
-	camera.capture('camera_test2.jpg')
-	print ("photo")
+	camera.resolution = (1024, 768) #set resolution 
+	camera.start_preview() #start preview screen 
+	time.sleep(2) # pause
+	camera.capture('camera_test2.jpg') # captures saves and names photo 
+	print ("photo") #'photo' displays when photo has been taken 
