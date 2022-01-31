221e6a9433b921a48a7d445d8b7163a992e4fe60
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2022-01-31 00:29:20 -0500

    commented camera code 02

--- Python/camera_test_02.py
+++ Python/camera_test_02.py
@@ -1,45 +1,45 @@
-import time
-import picamera
+import time # imports time libraries necessary for pauses 
+import picamera # imports camera libraries 
 
-print("running")
+print("Running") # displays constantly while camera is running 
 
 with picamera.PiCamera() as camera: 
-	camera.resolution = (1024, 768)
-	camera.image_effect = 'pastel' 
-	camera.start_preview()
-	time.sleep(2)
-	camera.capture('pastel_pic1.jpg')
-	print("picture 1: pastel")
+	camera.resolution = (1024, 768) # sets resolution 
+	camera.image_effect = 'pastel' # sets effect (pastel)
+	camera.start_preview() # starts preview screen 
+	time.sleep(2) # pause 
+	camera.capture('pastel_pic1.jpg') #takes saves and names picture
+	print("picture 1: pastel") # prints that picture 1 has been taken
 
 with picamera.PiCamera() as camera:
-        camera.resolution = (1024, 768)
-        camera.image_effect = 'cartoon'
-        camera.start_preview()
-        time.sleep(2)
-        camera.capture('cartoon_pic2.jpg')
-        print("picture 2: cartoon")
+        camera.resolution = (1024, 768) # sets resolution 
+        camera.image_effect = 'cartoon' # sets effect (cartoon)
+        camera.start_preview() # starts preview screen 
+        time.sleep(2) # pause
+        camera.capture('cartoon_pic2.jpg') # takes saves and names picture
+        print("picture 2: cartoon") # prints that picture 2 has been taken
 
 with picamera.PiCamera() as camera:
-        camera.resolution = (1024, 768)
-        camera.image_effect = 'negative'
-        camera.start_preview()
-        time.sleep(2)
-        camera.capture('negative_pic3.jpg')
-        print("picture 3: negative")
+        camera.resolution = (1024, 768) # sets resolution 
+        camera.image_effect = 'negative' # sets effect (negative)
+        camera.start_preview() # starts preview screen 
+        time.sleep(2) # pause 
+        camera.capture('negative_pic3.jpg') # takes saves and names picture
+        print("picture 3: negative") # prints that picture 3 has been taken 
 
 with picamera.PiCamera() as camera:
-        camera.resolution = (1024, 768)
-        camera.image_effect = 'oilpaint'
-        camera.start_preview()
-        time.sleep(2)
-        camera.capture('oilpaint_pic4.jpg')
-        print("picture 4: oilpaint")
+        camera.resolution = (1024, 768) # sets resolution 
+        camera.image_effect = 'oilpaint' # sets effect (oilpaint) 
+        camera.start_preview() # starts preview screen 
+        time.sleep(2) # pause 
+        camera.capture('oilpaint_pic4.jpg') # takes saves and names picture 
+        print("picture 4: oilpaint") # prints that picture 4 has been taken 
 
 with picamera.PiCamera() as camera:
-        camera.resolution = (1024, 768)
-        camera.image_effect = 'sketch'
-        camera.start_preview()
-        time.sleep(2)
-        camera.capture('sketch_pic5.jpg')
-        print("picture 5: sketch")
+        camera.resolution = (1024, 768) # sets resolution 
+        camera.image_effect = 'sketch' # sets effect (sketch)
+        camera.start_preview() # starts preview screen 
+        time.sleep(2) # pause
+        camera.capture('sketch_pic5.jpg') # takes saves and names picture
+        print("picture 5: sketch") # prints that picture 5 has been taken
 
